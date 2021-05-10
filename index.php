86d1fc06e4e14c502e127a20235b787845b072e4
Raesha Salsabila N.P <62698351+raesabila03@users.noreply.github.com>
2021-05-10 13:12:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <meta charset= "UTF-8">
+    <meta http-equiv= "X-UA-Compatible" content= "IE=edge">
+    <meta name= "viewport" content= "width=device-width, initial-scale=1.0">
+    <link rel= "stylesheet" href= "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity= "sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
     <style>
+        
         #mapid {
             height: 100%;
         }
@@ -13,10 +14,11 @@
 </head>
 
 <body>
-    <div id="mapid"></div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d996.647721185406!2d102.71853122915245!3d-2.3047251598748337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e2e2c1f4b0a080f%3A0x425663cfa2f45c18!2sLapangan%20Sriwijaya%2C%20Simpang%20Tiga%2C%20Sarolangun%2C%20Jambi!5e0!3m2!1sid!2sid!4v1620621142209!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <div id= "mapid"></div>
+    <iframe src= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d996.647721185406!2d102.71853122915245!3d-2.3047251598748337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e2e2c1f4b0a080f%3A0x425663cfa2f45c18!2sLapangan%20Sriwijaya%2C%20Simpang%20Tiga%2C%20Sarolangun%2C%20Jambi!5e0!3m2!1sid!2sid!4v1620621142209!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </body>
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+    
+<script src= "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 <script>
     var mymap = L.map('mapid').setView([-5.3928335, 105.2095938], 10);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
